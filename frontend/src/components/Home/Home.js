dc97082068a53b660fb856b87839579b8e32b660
cartercheng1996 <38425582+cartercheng1996@users.noreply.github.com>
2022-06-23 14:35:30 +1000

    Intergrate backend into Home.js

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,14 +1,28 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import Navbar from '../NavBar/Navbar';
 import LoggedNarbar from '../LoggedNavBar/Navbar'
+import { fetchDashboard } from '../../service.js';
+
 const Home = () => {
+  const [data, setData] = useState('');
   console.log(localStorage.getItem('sign-status'));
+
+  React.useEffect(async (e) => {
+    try {
+      const response = await fetchDashboard({ });
+      console.log(response.data.message.page);
+      setData(response.data.message.page);
+    } catch (error) {
+      alert(error);
+    }
+  }, []);
+
   return (
     <div className="home">
       {localStorage.getItem('sign-status')
         ? <LoggedNarbar></LoggedNarbar>
         : <Navbar></Navbar>}
+      <div>Get data from backend: {data}</div>
     </div>
   );
 }
--- frontend/src/service.js
+++ frontend/src/service.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://127.0.0.1:5000';
-
 // ADMIN AUTH
 export const register = (body) =>
   axios.post(`${BASE_URL}/regitser`, body);
 
 export const signIn = (body) =>
   axios.post(`${BASE_URL}/login`, body);
+
+export const fetchDashboard = () =>
+  axios.get('actors');
