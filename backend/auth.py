f67541f2a7f21948af8597c8afd474db5d376bce
yangbo <yuyangbo2410@gmail.com>
2022-06-27 12:41:34 +1000

    finished register

--- backend/auth.py
+++ backend/auth.py
@@ -20,6 +20,7 @@ cur.execute(
 app = Flask(__name__)
 api = Api(app)
 
+
 @api.route('/auth/register')
 class AuthRegister(Resource):
     def post(self):
@@ -40,48 +41,20 @@ class AuthRegister(Resource):
 
         # generate token, currently is just uuid
         # uuid4 is a random UUID
-        
-        uuid = uuid.uuid4()
+        token = str(uuid.uuid4())
 
         # passed all the check, insert into database
-        cur.execute("insert into users values (?, ?, ?, ?, ?)", [None, name, email, password, uuid])
+        cur.execute("insert into users values (?, ?, ?, ?, ?)",
+                    [None, name, email, password, token])
         con.commit()
 
-        return uuid, 200
+        return token, 200
 
 
 @api.route('/ping')
 class AuthRegister(Resource):
     def get(self):
         return "Pong", 200
-
-# def insert_in_sqlite(res):
-#     lst_to_insert = []
-#     for r in res:
-#         person = r["person"]
-#         user_id = person.get("id", "")
-#         # if id is already in table, skip this insert
-#         if id and check_id_exist(id):
-#             continue
-#         updated = person.get("updated", "")
-#         if updated:
-#             updated = convert_datetime(updated)
-#         name = person.get("name", "")
-#         country = person.get("country", "")
-#         if country:
-#             country = country.get("name", "")
-#         birthday = person.get("birthday", "")
-#         deathday = person.get("deathday", "")
-#         gender = person.get("gender", "")
-#         shows = get_shows_by_id(id)
-#         lst_to_insert.append(
-#             [id, updated, name, country, birthday, deathday, gender, shows])
-
-#     con = sqlite3.connect(DATABASE_NAME)
-#     cur = con.cursor()
-#     cur.executemany(
-#         "insert into actors values (?, ?, ?, ?, ?, ?, ?, ?)", lst_to_insert)
-#     con.commit()
 
 
 if __name__ == '__main__':
