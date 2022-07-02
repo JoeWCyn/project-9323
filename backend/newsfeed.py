c68857b267ff1080bdb51a9a51fc66192b5c1d18
yangbo3380 <107678837+yangbo3380@users.noreply.github.com>
2022-07-02 13:00:25 +1000

    added cors (#5)
    Co-authored-by: yangbo <yuyangbo2410@gmail.com>

--- backend/newsfeed.py
+++ backend/newsfeed.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
 from config import *
+from flask_cors import CORS
 
 newsfeed_page = Blueprint("newsfeed", __name__)
+CORS(newsfeed_page)
+
 
 @newsfeed_page.route('/newsfeed/ping', methods=['GET'])
 def newsfeed_ping():
