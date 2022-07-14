fc7adc566121a3fcdcd75e87dac0c0829879e595
JoeWCyn <100083131+JoeWCyn@users.noreply.github.com>
2022-07-14 19:18:49 +1000

    Create addSomeCodeIn_app.py
    I add some code in "app.py", and I don't have the right to access or modify the file, so please take the code into "app.py" in the Master branch or which branch will use in the presentation.

---
+++ backend/addSomeCodeIn_app.py
@@ -0,0 +1,89 @@
+from flask import Flask
+import sqlite3
+from config import *
+from auth import auth_page
+from newsfeed import newsfeed_page
+from question import question_page
+from comment import comment_page
+from article import article_page
+from score import score_page
+con = sqlite3.connect(DATABASE_NAME)
+cur = con.cursor()
+
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS users (id integer primary key autoincrement,\
+     name text, email text, password text, token text, expertArea text,\
+      scores integer, coins integer,expertOrNot NOT NULL DEFAULT '0',isPpublic NOT NULL DEFAULT '0')")
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS questions ( \
+        id integer primary key autoincrement, \
+        title text, \
+        content text, \
+        timeCreated numeric, \
+        timeUpdated numeric, \
+        author numeric, \
+        replyIds text, \
+        thumbUpBy text, \
+        isDeleted NOT NULL DEFAULT '0') \
+        ")
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS articles ( \
+        id integer primary key autoincrement, \
+        articleId numeric, \
+        stepNumber numeric, \
+        stepTitle text, \
+        title text, \
+        content text, \
+        image text, \
+        timeCreated numeric, \
+        timeUpdated numeric, \
+        author numeric, \
+        replyIds text, \
+        thumbUpBy text, \
+        isDeleted NOT NULL DEFAULT '0') \
+        ")
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS articles ( \
+        id integer primary key autoincrement, \
+        articleId numeric, \
+        stepNumber numeric, \
+        stepTitle text, \
+        title text, \
+        content text, \
+        image text, \
+        timeCreated numeric, \
+        timeUpdated numeric, \
+        author numeric, \
+        replyIds text, \
+        thumbUpBy text, \
+        isDeleted NOT NULL DEFAULT '0') \
+        ")
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS comments ( \
+        id integer primary key autoincrement, \
+        questionId numeric, \
+        articlesId numeric, \
+        content text, \
+        timeCreated numeric, \
+        timeUpdated numeric, \
+        author numeric, \
+        thumbUpBy text, \
+        isDeleted NOT NULL DEFAULT '0') \
+        ")
+
+app = Flask(__name__)
+app.register_blueprint(auth_page)
+app.register_blueprint(newsfeed_page)
+app.register_blueprint(question_page)
+app.register_blueprint(comment_page)
+app.register_blueprint(article_page)
+app.register_blueprint(score_page)
+
+@app.route("/ping")
+def ping():
+    return "pong", 200
+
+
+if __name__ == '__main__':
+
+    app.run(debug=DEBUG, port= PORT)
